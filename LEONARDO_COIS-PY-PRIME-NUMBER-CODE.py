15566512ab70378827745faec111fa351d5c02df
Leonardo Cois <53543163+LeonardoCois@users.noreply.github.com>
2019-09-07 12:15:37 -0300

    Update LEONARDO_COIS-PY-PRIME-NUMBER-CODE.py

--- LEONARDO_COIS-PY-PRIME-NUMBER-CODE.py
+++ LEONARDO_COIS-PY-PRIME-NUMBER-CODE.py
@@ -8,15 +8,22 @@ import ctypes
 kernel32 = ctypes.windll.kernel32
 kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
 
+# header
 tab1 = 60
 print('=' * tab1)
 print(f'{" PRIME NUMBERS ":-^{tab1}}')
 print('=' * tab1)
+
+# setting variables and list for db
 n = 0
 l = []
+
+# asking the inputs
 while True:
     x = str(input('>> Enter a integer number: '))
     spaces = len(x)
+    
+    # testing and cut above = 100
     if spaces < 3:
         print('>> Hey, let\'s do something fun, please try a bigger one, like 100')
     else:
@@ -28,8 +35,11 @@ while True:
     print()
 print('=' * tab1)
 print()
-# change for the tabulation
+
+# change this line for tabulation
 tab = 10
+
+# building organized and auto tab list
 for c in range(1, x + 1):
     sleep(0.05)
     if c % tab == 0:
@@ -44,6 +54,8 @@ for c in range(1, x + 1):
         ee = ' '
         leftm = ''
         rightm = ''
+        
+    # check if is prime ou not and add color
     if x % c == 0:
         print(f'{rightm}\033[1;32m{c: >{spaces}}\033[m{leftm}', end=ee)
         n += 1
@@ -54,6 +66,8 @@ l = '| ' + str(l)[1:-1].replace(',', ' |') + ' |'
 print()
 print('=' * tab1)
 print()
+
+# results and coloring
 if n == 1:
     res = '\033[1;34mIS PRIME NUMBER\033[m'
     s = ''
