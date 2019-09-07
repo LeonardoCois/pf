f467f5281f7f2d5a9531f972a655b731bbb6a9a4
Leonardo Cois <53543163+LeonardoCois@users.noreply.github.com>
2019-09-07 12:22:09 -0300

    Update LEONARDO_COIS-PY-LOTTO-GAME-CODE.py

--- LEONARDO_COIS-PY-LOTTO-GAME-CODE.py
+++ LEONARDO_COIS-PY-LOTTO-GAME-CODE.py
@@ -8,15 +8,20 @@ import ctypes
 kernel32 = ctypes.windll.kernel32
 kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
 
+# build variable and tabulation
 tab1 = 50
 ct1 = 1
 
+# header
 print(Fore.CYAN + '=' * tab1)
 print('{:^{}}'.format('LOTTO GAME DRAW', tab1))
 print('=' * tab1, '\n')
 print('Numbers from 1 to 60')
+
+# ask inputs
 while True:
     qst = str(input('How many games to draw? ')).strip()
+    # validate input
     try:
         qst = int(qst)
         break
@@ -27,15 +32,22 @@ print('=' * tab1)
 disp = f'RAFFLING {qst} GAMES'
 print(f'{disp:-^{tab1}}')
 print()
+
+# draw the game
 cl1 = 0
 while ct1 < qst + 1:
     l1 = []
+    # draw 6 numbers ==> from 1 to 60
     for i in range(0, 6):
         rnd = randint(1, 60)
+        # check for unique value
         while rnd in l1:
             rnd = randint(1, 60)
+        # adding to db and cleaning temp list
         l1.append(rnd)
         l1.sort()
+
+    # printing colored list
     if cl1 == 0:
         print(Fore.BLACK + Back.LIGHTGREEN_EX + f'>> Game {ct1:>2}: ', end='')
         cl1 = 1
@@ -44,7 +56,7 @@ while ct1 < qst + 1:
         cl1 = 0
     for c in l1:
         print(f'{c:>2}', end='   ')
-    print()    
+    print()
     ct1 += 1
     sleep(.5)
 print(Style.RESET_ALL)
