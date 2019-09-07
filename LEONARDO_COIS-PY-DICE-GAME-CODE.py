d3f57cd7d8f625bffbc9b4a71a16acb62031ce75
Leonardo Cois <53543163+LeonardoCois@users.noreply.github.com>
2019-09-07 12:06:34 -0300

    Update LEONARDO_COIS-PY-DICE-GAME-CODE.py

--- LEONARDO_COIS-PY-DICE-GAME-CODE.py
+++ LEONARDO_COIS-PY-DICE-GAME-CODE.py
@@ -1,4 +1,10 @@
 print('by Leonardo Cois\n' + '_' * 20 + '\n')
+
+# define printing for dice results
+def dice_print(clr, sps, post, qst2, v):
+    print(clr + '   ' + '-' * sps + f'{post} Position: Player({v["Player"]:0{qst2}d}) | dice: {v["dice"]}')
+
+
 from time import*
 from random import*
 
@@ -8,10 +14,12 @@ import ctypes
 kernel32 = ctypes.windll.kernel32
 kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
 
+# setting tabulation length and db
 tab1 = 80
 ply = {}
 data = []
 
+# header
 print(Fore.RED + '=' * tab1)
 print(f'{" AUTO DRAW - DICE GAME ":-^{tab1}}')
 print('=' * tab1)
@@ -19,8 +27,11 @@ print()
 print('I\'ll roll a dice for each player.')
 
 while True:
+    # asking for inputs
     qst1 = str(input('>> How many players? ')).strip()
     qst3 = str(input('>> How many sides for the dice? [press enter - default 6 sides] ')).strip()
+    
+    # testing inputs
     if qst3 == '':
         qst3 = 6
     qst2 = len(qst1)
@@ -32,6 +43,7 @@ while True:
         print('>> Error, try again')
         print()
 
+# roll dice, add to dic
 print()
 print(Fore.GREEN + '-' * tab1)
 for c in range(1, qst1 + 1):
@@ -39,22 +51,30 @@ for c in range(1, qst1 + 1):
     ply['dice'] = randint(1, qst3)
     sleep(0.5)
     print(f'>> Player({ply["Player"]:0{qst2}d}) took in dice: {ply["dice"]}')
+    
+    # add data from dict to db/list and ranking
+    # if is empty
     if data == []:
         data.append(ply.copy())
     else:
         for p, v in enumerate(data):
+            # check if first item
             if ply['dice'] < v['dice'] and p + 1 == len(data):
                 data.append(ply.copy())
                 break
+            # roll over the list to find position
             elif ply['dice'] < v['dice']:
-                pass                
+                pass
+            # insert in correct ranking pos
             else:
                 data.insert(p, ply.copy())
                 break
+# header for ranking
 print(Fore.GREEN + '-' * tab1)
 print('   --== RANKING POSITION ==--   ')
 print()
 
+# setting variables
 pos = 1
 post = '1st'
 sps = 0
@@ -62,11 +82,13 @@ clr = Fore.YELLOW
 
 for p, v in enumerate(data):
     sleep(.5)
+    # building colored and tabulated list by it position
     if p == 0:
-        print(clr + '   ' + '-' * sps + f'{post} Position: Player({v["Player"]:0{qst2}d}) | dice: {v["dice"]}')
+        dice_print(clr, sps, post, qst2, v)
     elif v['dice'] == data[p - 1]['dice']:
-        print(clr + '   ' + '-' * sps + f'{post} Position: Player({v["Player"]:0{qst2}d}) | dice: {v["dice"]}')
+        dice_print(clr, sps, post, qst2, v)
     else:
+        # ordinal number
         pos += 1
         if pos == 1:
             post = '1st'
@@ -84,8 +106,8 @@ for p, v in enumerate(data):
             clr = Fore.LIGHTWHITE_EX
             sps += 2
         if pos == 4:
-            print(clr + '-' * len(clr + '   ' + '-' * sps + f'{post} Position: Player({v["Player"]:0{qst2}d}) | dice: {v["dice"]}'))
-        print(clr + '   ' + '-' * sps + f'{post} Position: Player({v["Player"]:0{qst2}d}) | dice: {v["dice"]}')
+            print(clr + '-' * len(dice_print(clr, sps, post, qst2, v)))
+        dice_print(clr, sps, post, qst2, v)
 print(Style.RESET_ALL + '-' * tab1)
 print()
 ex = input('press enter to exit...')
